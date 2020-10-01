8fabf51ccdc93b9e7d86eff09217b2aaf537b784
Christopher Barnes <chris.barnes.2000@me.com>
2020-09-30 20:39:31 -0700

    [Add]: Headder Component

---
+++ make-badges-backend/src/Title.js
@@ -0,0 +1,18 @@
+// src/Title.js
+import React from "react";
+import "./Title.css";
+
+function Title() {
+  return (
+    <div className="Title">
+      <header>
+        <h1>Make School Badges</h1>
+        <div className="Title-Subtitle">
+          <h2>Sub-Title</h2>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Title;
