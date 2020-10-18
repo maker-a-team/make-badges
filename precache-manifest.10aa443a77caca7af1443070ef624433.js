7f2fcfab03531a3a606552385ba3179d4ad97a12
Christopher Barnes <chris.barnes.2000@me.com>
2020-10-18 01:09:31 -0700

    Updates

---
+++ precache-manifest.10aa443a77caca7af1443070ef624433.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "440e364580e4c03e37dc42c7f9c06192",
+    "url": "/make-badges/index.html"
+  },
+  {
+    "revision": "3aa094ea254773b129f8",
+    "url": "/make-badges/static/css/main.46b83d97.chunk.css"
+  },
+  {
+    "revision": "52d2b9e4429fa79f7cd3",
+    "url": "/make-badges/static/js/2.5fa2691d.chunk.js"
+  },
+  {
+    "revision": "9012292ef7455cc721fc02cd63a229d1",
+    "url": "/make-badges/static/js/2.5fa2691d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa094ea254773b129f8",
+    "url": "/make-badges/static/js/main.b2a4e1d4.chunk.js"
+  },
+  {
+    "revision": "4482d29b041a38302fe3",
+    "url": "/make-badges/static/js/runtime-main.81fd5d43.js"
+  },
+  {
+    "revision": "759f6c70b4c0b6f3777380cb068ddc53",
+    "url": "/make-badges/static/media/logo-white.759f6c70.svg"
+  }
+]);
