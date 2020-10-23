3a18fbaf76fa7076d242c5324b15db107aa3e99f
Christopher Barnes <chris.barnes.2000@me.com>
2020-10-22 18:37:16 -0700

    Updates

---
+++ precache-manifest.6aa8631a62435b3e52b1bc0e49cb7492.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c86b1d7997743356b1632c264eadaf4",
+    "url": "/make-badges/index.html"
+  },
+  {
+    "revision": "036d090f097d0c3abf4c",
+    "url": "/make-badges/static/css/main.b984aedc.chunk.css"
+  },
+  {
+    "revision": "b8d1658da92ee6b4b069",
+    "url": "/make-badges/static/js/2.fee0956e.chunk.js"
+  },
+  {
+    "revision": "9012292ef7455cc721fc02cd63a229d1",
+    "url": "/make-badges/static/js/2.fee0956e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "036d090f097d0c3abf4c",
+    "url": "/make-badges/static/js/main.5ceb315d.chunk.js"
+  },
+  {
+    "revision": "4482d29b041a38302fe3",
+    "url": "/make-badges/static/js/runtime-main.81fd5d43.js"
+  },
+  {
+    "revision": "759f6c70b4c0b6f3777380cb068ddc53",
+    "url": "/make-badges/static/media/logo-white.759f6c70.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.960792e816bd8dbb45ad36d4e45d639a.js"
+  "/make-badges/precache-manifest.6aa8631a62435b3e52b1bc0e49cb7492.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/make-badges/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
