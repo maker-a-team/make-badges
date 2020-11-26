97c55114a24ca102d798796d6616b94ed6a225c6
Christopher Barnes <chris.barnes.2000@me.com>
2020-11-25 17:44:33 -0800

    [Update]: Firebase config

--- src/Components/Firebase/firebase.js
+++ src/Components/Firebase/firebase.js
@@ -5,25 +5,15 @@ import 'firebase/storage';
 import 'firebase/analytics';
 
 
-// const config = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-// };
-
-
 const config = {
-  apiKey: "AIzaSyBpxbPor7YSFc5J6q0lw79CVXYYAjLvlos",
-  authDomain: "makeschoolbadges.firebaseapp.com",
-  databaseURL: "https://makeschoolbadges.firebaseio.com",
-  projectId: "makeschoolbadges",
-  storageBucket: "makeschoolbadges.appspot.com",
-  messagingSenderId: "360872796104",
-  appId: "1:360872796104:web:7692a1e1e0c89e77c16afd",
-  measurementId: "G-0F7RZM57QH",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 
